33c727186da40e2e53181441f1ca92bc5074cdec
Alexis Edwards <56770721+aedwards4@users.noreply.github.com>
2021-11-16 18:42:26 -0600

    My Bookings tab functionality complete

--- PetSitterApp/PetSitterApp/Controller/MyBookingsViewController.swift
+++ PetSitterApp/PetSitterApp/Controller/MyBookingsViewController.swift
@@ -50,9 +50,33 @@ class MyBookingsViewController: UIViewController, UITableViewDelegate, UITableVi
         let cell = tableView.dequeueReusableCell(withIdentifier: "bookingCell") as! BookingCell
         
         let booking = self.appointments[indexPath.row]
-        let sitter = booking["sitter"]
+        let sitter = booking["sitter"] as! PFObject
+        let sitterFName = sitter["firstName"] as! String
+        let sitterLName = sitter["lastName"] as! String
+        print("Sitter is: \(sitter)")
+        
+        cell.sitterName.text = sitterFName + " " + sitterLName
+        
+        let formatter = DateFormatter()
+//        formatter.dateFormat = "MM/dd/yyyy HH:mm"
+        formatter.dateFormat = "MM-dd-yyyy"
+        let myString = formatter.string(from: (booking["start"] as! NSDate) as Date)
+        cell.startDate.text = myString
+        
+        if (sitter["profileImage"] != nil) {
+            let imageFile = sitter["profileImage"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
+            cell.sitterPhoto.af_setImage(withURL: url)
+        }
+        
+        cell.petNames.text = "Morty, Minerva"
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
     }
 
 }
